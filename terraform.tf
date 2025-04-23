0a6fee9f66166bd6efd80995923c52a867ad4f5b
Zach <zach.allen@hashicorp.com>
2025-04-23 11:36:18 -0500

    Update provider config and lookup

---
+++ terraform.tf
@@ -0,0 +1,26 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  cloud {
+    workspaces {
+      name = "pipeline-k8s"
+      project = "sandbox"
+    }
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.77.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+
+
+provider "google" {
+  project = var.google_project
+  region  = var.region
+}
