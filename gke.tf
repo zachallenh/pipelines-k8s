5af67b6b2d7059a0948d61307e85c93be2d354dd
Zach <zach.allen@hashicorp.com>
2025-04-25 18:23:38 -0500

    Remove old config and turn off deletion protectoin

--- gke.tf
+++ gke.tf
@@ -12,6 +12,8 @@ resource "google_container_cluster" "engineering" {
   name     = var.cluster_name
   location = data.google_compute_zones.available.names.0
 
+  deletion_protection = true
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
@@ -19,38 +21,4 @@ resource "google_container_cluster" "engineering" {
   initial_node_count       = 1
 
   ip_allocation_policy {}
-}
-
-resource "google_container_node_pool" "engineering_preemptible_nodes" {
-  name     = "${var.cluster_name}-node-pool"
-  cluster  = google_container_cluster.engineering.name
-  location = data.google_compute_zones.available.names.0
-
-  version    = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
-  node_count = var.node_count
-
-  node_config {
-    preemptible  = true
-    machine_type = "n1-standard-1"
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-    kubelet_config {
-      cpu_cfs_quota = false
-      cpu_cfs_quota_period = ""
-      cpu_manager_policy = ""
-      pod_pids_limit = 0
-    }
-
-    # reservation_affinity {
-    #   consume_reservation_type = "NO_RESERVATION"
-    # }
-  }
 }
